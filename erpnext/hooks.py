5a122704b60689b4e09d27f0a781491daabf7346
Eng Acs <me@engacs.pw>
2024-06-19 17:05:15 +0300

    Update hooks.py

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -7,7 +7,7 @@ app_color = "#e74c3c"
 app_email = "info@erpnext.com"
 app_license = "GNU General Public License (v3)"
 source_link = "https://github.com/frappe/erpnext"
-app_logo_url = "/assets/erpnext/images/erpnext-logo.svg"
+app_logo_url = "/assets/erpnext/images/easytouch-logo.svg"
 
 
 develop_version = "14.x.x-develop"
