cfa432dbcae7c5bfec6ec8be83229211de9c639c
Sanket322 <shahsanket322003.com>
2024-12-25 18:14:10 +0530

    fix: set/update billing address on change of company
    (cherry picked from commit 0adfebee85c58bf0585b259f28f803ad852af182)

--- erpnext/public/js/controllers/transaction.js
+++ erpnext/public/js/controllers/transaction.js
@@ -959,7 +959,6 @@ erpnext.TransactionController = class TransactionController extends erpnext.taxe
 				let is_drop_ship = me.frm.doc.items.some(item => item.delivered_by_supplier);
 
 				if (!is_drop_ship) {
-					console.log('get_shipping_address');
 					erpnext.utils.get_shipping_address(this.frm, function() {
 						set_party_account(set_pricing);
 					});
