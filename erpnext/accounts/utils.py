18946f84955286b89708860e5e4f9884b87477bf
ruthra kumar <ruthra@erpnext.com>
2025-01-14 13:52:21 +0530

    refactor: only update `reconcile_effect_on` advance in separate acc
    (cherry picked from commit 9ee5fcc602f1dae492d06b85220564c889f49c3a)

--- erpnext/accounts/utils.py
+++ erpnext/accounts/utils.py
@@ -713,20 +713,21 @@ def update_reference_in_payment_entry(
 	update_advance_paid = []
 
 	# Update Reconciliation effect date in reference
-	if payment_entry.advance_reconciliation_takes_effect_on == "Advance Payment Date":
-		reconcile_on = payment_entry.posting_date
-	elif payment_entry.advance_reconciliation_takes_effect_on == "Oldest Of Invoice Or Advance":
-		date_field = "posting_date"
-		if d.against_voucher_type in ["Sales Order", "Purchase Order"]:
-			date_field = "transaction_date"
-		reconcile_on = frappe.db.get_value(d.against_voucher_type, d.against_voucher, date_field)
-
-		if getdate(reconcile_on) < getdate(payment_entry.posting_date):
+	if payment_entry.book_advance_payments_in_separate_party_account:
+		if payment_entry.advance_reconciliation_takes_effect_on == "Advance Payment Date":
 			reconcile_on = payment_entry.posting_date
-	elif payment_entry.advance_reconciliation_takes_effect_on == "Reconciliation Date":
-		reconcile_on = nowdate()
+		elif payment_entry.advance_reconciliation_takes_effect_on == "Oldest Of Invoice Or Advance":
+			date_field = "posting_date"
+			if d.against_voucher_type in ["Sales Order", "Purchase Order"]:
+				date_field = "transaction_date"
+			reconcile_on = frappe.db.get_value(d.against_voucher_type, d.against_voucher, date_field)
 
-	reference_details.update({"reconcile_effect_on": reconcile_on})
+			if getdate(reconcile_on) < getdate(payment_entry.posting_date):
+				reconcile_on = payment_entry.posting_date
+		elif payment_entry.advance_reconciliation_takes_effect_on == "Reconciliation Date":
+			reconcile_on = nowdate()
+
+		reference_details.update({"reconcile_effect_on": reconcile_on})
 
 	if d.voucher_detail_no:
 		existing_row = payment_entry.get("references", {"name": d["voucher_detail_no"]})[0]
