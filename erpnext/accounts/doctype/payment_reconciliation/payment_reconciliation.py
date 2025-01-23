ccaf0d4b858b5fe65863a0da2c84eab3d19bb882
ruthra kumar <ruthra@erpnext.com>
2025-01-23 14:10:15 +0530

    refactor: only apply configuration on normal payments
    patch to update default value
    
    (cherry picked from commit b2c3da135ea85fe245ec9c6066a8e68b42b64f7f)

--- erpnext/accounts/doctype/payment_reconciliation/payment_reconciliation.py
+++ erpnext/accounts/doctype/payment_reconciliation/payment_reconciliation.py
@@ -335,6 +335,7 @@ class PaymentReconciliation(Document):
 		for payment in non_reconciled_payments:
 			row = self.append("payments", {})
 			row.update(payment)
+			row.is_advance = payment.book_advance_payments_in_separate_party_account
 
 	def get_invoice_entries(self):
 		# Fetch JVs, Sales and Purchase Invoices for 'invoices' to reconcile against
@@ -453,10 +454,11 @@ class PaymentReconciliation(Document):
 				res.difference_account = default_exchange_gain_loss_account
 				res.exchange_rate = inv.get("exchange_rate")
 				res.update({"gain_loss_posting_date": pay.get("posting_date")})
-				if exc_gain_loss_posting_date == "Invoice":
-					res.update({"gain_loss_posting_date": inv.get("invoice_date")})
-				elif exc_gain_loss_posting_date == "Reconciliation Date":
-					res.update({"gain_loss_posting_date": nowdate()})
+				if not pay.get("is_advance"):
+					if exc_gain_loss_posting_date == "Invoice":
+						res.update({"gain_loss_posting_date": inv.get("invoice_date")})
+					elif exc_gain_loss_posting_date == "Reconciliation Date":
+						res.update({"gain_loss_posting_date": nowdate()})
 
 				if pay.get("amount") == 0:
 					entries.append(res)
--- erpnext/controllers/accounts_controller.py
+++ erpnext/controllers/accounts_controller.py
@@ -2969,6 +2969,7 @@ def get_advance_payment_entries(
 			(payment_ref.allocated_amount).as_("amount"),
 			(payment_ref.name).as_("reference_row"),
 			(payment_ref.reference_name).as_("against_order"),
+			(payment_entry.book_advance_payments_in_separate_party_account),
 		)
 
 		q = q.where(payment_ref.reference_doctype == order_doctype)
@@ -3013,6 +3014,7 @@ def get_common_query(
 			(payment_entry.name).as_("reference_name"),
 			payment_entry.posting_date,
 			(payment_entry.remarks).as_("remarks"),
+			(payment_entry.book_advance_payments_in_separate_party_account),
 		)
 		.where(payment_entry.payment_type == payment_type)
 		.where(payment_entry.party_type == party_type)
